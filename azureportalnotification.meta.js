466636e627f13db80b7e7d1a2ccad7e6d63cd99f
horihiro <hori.kawa.shima.tool+github@gmail.com>
2020-03-24 14:47:45 +0900

    first commit

---
+++ azureportalnotification.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Azure Portal Notification
+// @namespace    http://horihiro.net/
+// @version      0.1
+// @description  Azure Portal Notification
+// @author       horihiro
+// @match        https://portal.azure.com/*
+// @grant        none
+// ==/UserScript==
