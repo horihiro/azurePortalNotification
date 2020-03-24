97ad949431fde02e2d12f6025f1866e78875bf8c
horihiro <hori.kawa.shima.tool+github@gmail.com>
2020-03-24 14:48:41 +0900

    update url

--- azureportalnotification.meta.js
+++ azureportalnotification.meta.js
@@ -5,5 +5,7 @@
 // @description  Azure Portal Notification
 // @author       horihiro
 // @match        https://portal.azure.com/*
+// @downloadURL  https://raw.githubusercontent.com/horihiro/azurePortalNotification/master/azureportalnotification.user.js
+// @updateURL    https://raw.githubusercontent.com/horihiro/azurePortalNotification/master/azureportalnotification.meta.js
 // @grant        none
 // ==/UserScript==
